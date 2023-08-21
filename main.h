62e5a61857a6fa517219a9275ef3bf2811dde9d9
Colightech <saintchris@gmail.com>
2023-08-21 06:52:10 -0700

    my printf project

--- main.h
+++ main.h
@@ -4,8 +4,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
-
+int _puts(char *z);
+int _putcha(char c);
 
 #endif

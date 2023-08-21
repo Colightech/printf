16f7a5bc916bd50f2f5f1683250b8d659d2a10fc
Colightech <saintchris@gmail.com>
2023-08-21 09:28:24 -0700

    my printf project

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	int slen, ch, x, print_ch = 0;
 	va_list arglist;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(arglist, format);
 

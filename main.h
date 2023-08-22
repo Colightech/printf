3acf728c2f6d5b89f7afe95c91bccbf69f7d8aa8
Colightech <saintchris@gmail.com>
2023-08-22 02:52:52 -0700

    my printf project

--- main.h
+++ main.h
@@ -4,11 +4,25 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 int _printf(const char *format, ...);
-int _puts(char *z);
-int _putcha(char c);
+int (*find_specifiers(const char *format))(va_list);
+
+/**
+ * struct func - for specifier to pointer
+ * @t: character to check
+ * @f: function pointer
+ */
+
+typedef struct func
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
+
+int print_ch(va_list arglist);
+int print_str(va_list arglist);
+int print_pcent(va_list arglist);
+
 
 #endif
